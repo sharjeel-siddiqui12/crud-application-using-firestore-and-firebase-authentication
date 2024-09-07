63606c793a9e9e73d6382317da795e62c4253dce
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2024-09-08 02:30:18 +0500

    refactor: Update Login component and add Facebook login functionality

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,57 @@
 import React, { useState } from "react";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  FacebookAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  
+
+  //  facebook login handler
+  const facebookLoginHandler = () => {
+    
+    // setting up facebook login
+    const provider = new FacebookAuthProvider();
+    // provider.addScope("user_birthday");
+    provider.setCustomParameters({
+      display: "popup",
+    });
+
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user;
+
+        console.log("facebook login success", user);
+
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const accessToken = credential.accessToken;
+
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+
+        console.log("facebook login error", error);
+
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = FacebookAuthProvider.credentialFromError(error);
+
+        // ...
+      });
+  };
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -83,6 +131,13 @@ const Login = () => {
           </button>
         </div>
       </form>
+      <br />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        onClick={facebookLoginHandler}
+      >
+        Login with Facebook
+      </button>
     </div>
   );
 };
